bd65cfb7e8dd5988c88fe0f9c9b159be309341f7
rstatistics <admin@ncrna.net>
2020-06-04 23:28:27 +0800

    upload rmats-issue

---
+++ run_rmats_4.0.2.sh
@@ -0,0 +1,1 @@
+/usr/bin/python rMATS.4.0.2/rMATS-turbo-Linux-UCS4/rmats.py --b1 b1.txt --b2 b2.txt --gtf ./gene.gtf --od ./rmats_4.0.2_output 2>&1 > Log_rmats_4.0.2.txt
---
+++ run_rmats_4.1.0.sh
@@ -0,0 +1,1 @@
+python rmats-turbo/rmats.py --b1 b1.txt --b2 b2.txt --gtf ./gene.gtf --od ./rmats_4.1.0_output --tmp ./tmp 2>&1 1> Log_rmats_4.1.0.txt
